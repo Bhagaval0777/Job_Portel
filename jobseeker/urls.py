cd31f4d95d262442025bd164e53992e35e583b1b
Sachin-Krishnan <sachin.s@vaisesika.in>
2026-04-21 11:04:33 +0530

    job seeker_frondent

--- jobseeker/urls.py
+++ jobseeker/urls.py
@@ -5,4 +5,6 @@ from . import views  # This imports the view we made earlier
 urlpatterns = [
     # This matches: http://127.0.0.1:8000/jobseeker/profile/
     path('profile/', views.profile_view, name='profile_view'),
+    path('edit-profile/', views.edit_profile_view, name='edit_profile_view'),
+
 ]
