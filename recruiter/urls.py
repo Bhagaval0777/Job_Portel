f491808969745dfc8289248b178a5bc21d7aa6ca
GokulDevS <gokuldevs1818@gmail.com>
2026-05-05 15:16:39 +0530

    Update urls.py

--- recruiter/urls.py
+++ recruiter/urls.py
@@ -1,11 +1,37 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from .views import CompanyViewSet, RecruiterViewSet
 
-router = DefaultRouter()
-router.register(r'companies', CompanyViewSet)
-router.register(r'profiles', RecruiterViewSet)
+# Defining the app name for namespacing
+app_name = 'profiles'
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # --- Company URLs ---
+    # Matches: /companies/
+    path('companies/', CompanyViewSet.as_view({
+        'get': 'list', 
+        'post': 'create'
+    }), name='company-list'),
+    
+    # Matches: /companies/<id>/
+    path('companies/<int:pk>/', CompanyViewSet.as_view({
+        'get': 'retrieve', 
+        'put': 'update', 
+        'patch': 'partial_update', 
+        'delete': 'destroy'
+    }), name='company-detail'),
+
+    # --- Recruiter URLs ---
+    # Matches: /profiles/
+    path('profiles/', RecruiterViewSet.as_view({
+        'get': 'list', 
+        'post': 'create'
+    }), name='recruiter-list'),
+    
+    # Matches: /profiles/<id>/
+    path('profiles/<int:pk>/', RecruiterViewSet.as_view({
+        'get': 'retrieve', 
+        'put': 'update', 
+        'patch': 'partial_update', 
+        'delete': 'destroy'
+    }), name='recruiter-detail'),
 ]
