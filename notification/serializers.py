4ac40de7f95cf6c7f68c9e54dd2c1b65178c9405
Manikandadaska075 <manikanda.das@vaisesika.in>
2026-05-14 12:13:13 +0530

    user update

--- notification/serializers.py
+++ notification/serializers.py
@@ -2,8 +2,41 @@ from rest_framework import serializers
 
 from .models import Notification
 
-class NotificationSerializer(serializers.ModelSerializer):
+# class NotificationSerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = Notification
+#         fields = '__all__'
+
+    
+class NotificationListSerializer(serializers.ModelSerializer):
+    time = serializers.SerializerMethodField()
 
     class Meta:
         model = Notification
-        fields = '__all__'
+        fields = ["id", "message", "time"]
+
+    def get_time(self, obj):
+        from django.utils.timesince import timesince
+        from django.utils import timezone
+        return timesince(obj.created_at, timezone.now()) + " ago"
+
+class NotificationSerializer(serializers.ModelSerializer):
+    time = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Notification
+        fields = [
+            "id",
+            "title",
+            "message",
+            "notification_type",
+            "is_read",
+            "read_at",
+            "time"
+        ]
+
+    def get_time(self, obj):
+        from django.utils.timesince import timesince
+        from django.utils import timezone
+        return timesince(obj.created_at, timezone.now()) + " ago"
--- notification/views.py
+++ notification/views.py
@@ -4,26 +4,15 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListAPIView
 from .models import Notification
-from .serializers import NotificationSerializer  
+from .serializers import NotificationSerializer , NotificationListSerializer
+from rest_framework import status
 
 class NotificationListView(ListAPIView):
-    serializer_class = (NotificationSerializer)
+    serializer_class = NotificationListSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Notification.objects.filter(recipient=self.request.user)
-
-class MarkNotificationReadView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, pk):
-        notification = (Notification.objects.get(pk=pk, recipient=request.user))
-        notification.is_read = True
-        notification.read_at = (timezone.now())
-        notification.save(update_fields=['is_read', 'read_at'])
-        return Response({
-            'success': True
-        })
+        return Notification.objects.filter(recipient=self.request.user).order_by('is_read', '-created_at')   
 
 class NotificationUnreadCountView(APIView):
     permission_classes = [IsAuthenticated]
@@ -34,3 +23,30 @@ class NotificationUnreadCountView(APIView):
         return Response({
             'unread_count': (unread_count)
             })
+
+class MarkNotificationReadView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request, pk):
+        try:
+            notification = Notification.objects.get(
+                pk=pk,
+                recipient=request.user
+            )
+        except Notification.DoesNotExist:
+            return Response(
+                {"error": "Notification not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        if not notification.is_read:
+            notification.is_read = True
+            notification.read_at = timezone.now()
+            notification.save(update_fields=["is_read", "read_at"])
+
+        serializer = NotificationSerializer(notification)
+
+        return Response({
+            "success": True,
+            "notification": serializer.data
+        })
