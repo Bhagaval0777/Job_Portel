c354a7d7f55be959d1a73486edaf55fac3ba4a48
Manikandadaska075 <manikanda.das@vaisesika.in>
2026-05-14 14:39:11 +0530

    subscription

--- Users/views.py
+++ Users/views.py
@@ -14,8 +14,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
-
-
 from axes.handlers.proxy import AxesProxyHandler
 
 # Local/App Imports
